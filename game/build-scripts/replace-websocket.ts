4abde0ab746a8e47e8d994ffcf06a354771118bb
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-12-21 16:55:47 +0100

    chore: update build.yml

--- game/build-scripts/replace-websocket.ts
+++ game/build-scripts/replace-websocket.ts
@@ -1,12 +1,18 @@
-import { readFileSync, writeFileSync } from 'node:fs';
-import { join } from 'path';
+import { readFileSync, writeFileSync } from "node:fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 
-const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || '3000';
-const socketFile = join(__dirname, '../.godot/GodotJS/src/socket-singleton.js');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || "3000";
+const socketFile = join(__dirname, "../.godot/GodotJS/src/socket-singleton.js");
 
 console.log(`Updating socket file to ${host}:${port}`);
 
-const content = readFileSync(socketFile, 'utf8');
-const updated = content.replace(/ws:\/\/localhost:3000/g, `ws://${host}:${port}`);
+const content = readFileSync(socketFile, "utf8");
+const updated = content.replace(
+  /ws:\/\/localhost:3000/g,
+  `ws://${host}:${port}`,
+);
 writeFileSync(socketFile, updated);
