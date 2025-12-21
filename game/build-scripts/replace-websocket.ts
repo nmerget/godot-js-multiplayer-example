bc2fbd15f0e8453a53e99ed2af933357b17dc419
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-12-21 16:58:27 +0100

    fix: issue with replace-websocket.ts

--- game/build-scripts/replace-websocket.ts
+++ game/build-scripts/replace-websocket.ts
@@ -1,18 +1,15 @@
 import { readFileSync, writeFileSync } from "node:fs";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || "3000";
-const socketFile = join(__dirname, "../.godot/GodotJS/src/socket-singleton.js");
+const protocol = process.env.PROTOCOL || "ws";
+const socketFile = ".godot/GodotJS/src/socket-singleton.js";
 
 console.log(`Updating socket file to ${host}:${port}`);
 
 const content = readFileSync(socketFile, "utf8");
 const updated = content.replace(
   /ws:\/\/localhost:3000/g,
-  `ws://${host}:${port}`,
+  `${protocol}://${host}:${port}`,
 );
 writeFileSync(socketFile, updated);
