f7fa2e3b6c2a5f366c772398904aa38562394440
Nicolas Merget <nicolas.merget@deutschebahn.com>
2026-01-02 13:53:33 +0100

    fix: issue with copy-build

--- game/build-scripts/replace-websocket.ts
+++ game/build-scripts/replace-websocket.ts
@@ -1,25 +1,15 @@
 import { readFileSync, writeFileSync } from "node:fs";
-import { resolve, dirname } from "path";
-import { fileURLToPath } from "url";
-import { cpSync } from "fs";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || "3000";
 const protocol = process.env.PROTOCOL || "ws";
-const socketFile = ".godot/GodotJS/src/socket-singleton.js";
+const socketFile = "./src/socket-singleton.ts";
 
-console.log(`Updating socket file to ${host}:${port}`);
+console.log(`Updating socket file to ${protocol}://${host}:${port}`);
 
 const content = readFileSync(socketFile, "utf8");
-const updated = content.replace(
-  /ws:\/\/localhost:3000/g,
+const updated = content.replaceAll(
+  "ws://localhost:3000",
   `${protocol}://${host}:${port}`,
 );
 writeFileSync(socketFile, updated);
-
-const backendDistPath = resolve(__dirname, "../../backend/dist/build");
-const gamePath = resolve(__dirname, "../../game");
-const buildPath = resolve(gamePath, "build");
-cpSync(buildPath, backendDistPath, { recursive: true });
